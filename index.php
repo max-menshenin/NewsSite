0b56fd4d982298ce8d653cb64da243d75d136575
postmind <max.menshenin.dev@yandex.ru>
2021-01-06 16:48:44 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>PHP Blog</title>
+</head>
+<body>
+<header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <p class="h5 my-0 me-md-auto fw-normal">PHP Dev</p>
+    <nav class="my-2 my-md-0 me-md-3">
+        <a class="p-2 text-dark" href="#">Main</a>
+        <a class="p-2 text-dark" href="#">Contacts</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="#">Sign up</a>
+</header>
+<div class="container mt-5">
+    <h3>Our articles</h3>
+    <div class="d-flex flex wrap">
+        <?php
+        for($i=0;$i < 5;$i++):?>
+            <div class="col">
+                <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                        <h4 class="my-0 fw-normal">Free</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>10 users included</li>
+                            <li>2 GB of storage</li>
+                            <li>Email support</li>
+                            <li>Help center access</li>
+                        </ul>
+                        <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+                    </div>
+                </div>
+            </div>
+        <?php endfor;?>
+    </div>
+</div>
+
+
+
+</body>
+</html>
